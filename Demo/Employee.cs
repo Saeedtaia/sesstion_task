afcadb23548ac15ca063de4ad0dab9b613e47692
Abo Tia3 <Abo Tia3@S3EED>
2024-07-08 05:41:13 +0300

    Add project files.

---
+++ Demo/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal struct Employee
+    {
+        public bool ValidateData()
+        {
+            return true;
+        }
+        private int id { get; set; }
+        private string name { get; set; }
+        private int salary { get; set; }
+        private Gender gender { get; set; }
+
+
+
+        public Employee(int id, string name, int salary, Gender gender)
+        {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+            this.gender = gender;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Your name : {name} \nYour id  : {id} \nyour salary : {salary}\nyour Gender : {gender}";
+        }
+    }
+    
+   
+    internal enum Gender
+    {
+        Male,
+        Female,
+    }
+
+
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+     class Program
+    {
+        static void Main()
+        {
+            Employee employee1 = new Employee(1, "ahmed" , 5000 , Gender.Male);
+            if (employee1.ValidateData())
+            {
+                Console.WriteLine("Data is valid.");
+            }
+            else
+            {
+                Console.WriteLine("Data is invalid.");
+            }
+        }
+    }
+}
